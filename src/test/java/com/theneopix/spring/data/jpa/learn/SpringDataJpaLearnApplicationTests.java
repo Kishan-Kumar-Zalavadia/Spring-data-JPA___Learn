069fcedbf7d130a1d61c4147c689f30b635522d2
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-02-02 09:38:46 +0530

    init

---
+++ src/test/java/com/theneopix/spring/data/jpa/learn/SpringDataJpaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theneopix.spring.data.jpa.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
